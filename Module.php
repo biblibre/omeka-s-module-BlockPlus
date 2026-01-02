e64ff3b6f51f4fdb930bdf1fe2bbf4558887dfba
Thibaud Guillot <thibaud.guillot@biblibre.com>
2026-01-02 10:40:19 +0100

    Rebasing 3.4.22

--- Module.php
+++ Module.php
@@ -32,14 +32,6 @@ class Module extends AbstractModule
         $translate = $plugins->get('translate');
         $translator = $services->get('MvcTranslator');
 
-        if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.58')) {
-            $message = new \Omeka\Stdlib\Message(
-                $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
-                'Common', '3.4.58'
-            );
-            throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
-        }
-
         $js = __DIR__ . '/asset/vendor/ThumbnailGridExpandingPreview/js/grid.js';
         if (!file_exists($js)) {
             $message = new PsrMessage(
